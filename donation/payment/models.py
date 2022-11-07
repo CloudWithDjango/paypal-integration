6622749a017f8cfa4f6b28b8f4693e4f75680443
CloudWithDjango <clouddeveloper96@gmail.com>
2022-11-07 13:17:30 +0400

    Code upload.
    Final code for paypal-integration project.

---
+++ donation/payment/models.py
@@ -0,0 +1,28 @@
+from unittest.util import _MAX_LENGTH
+from django.db import models
+
+# Create your models here.
+
+class Donation(models.Model):
+
+    title = models.CharField(max_length=200)
+
+    description = models.TextField(max_length=300)
+
+    amount = models.DecimalField(max_digits=3, decimal_places=2)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
